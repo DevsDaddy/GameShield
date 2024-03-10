a29a07d485ebcd9b56702baa458bbc944364f3a8
Ilya Rastrorguev <i.rastorguev@outlook.com>
2024-03-10 20:51:58 +0500

    Add SpeedHack Detector Module

--- Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Modules/SpeedHack/SpeedHackDetector.cs
+++ Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Modules/SpeedHack/SpeedHackDetector.cs
@@ -1,3 +1,5 @@
+using System;
+using DevsDaddy.GameShield.Core.Constants;
 using DevsDaddy.GameShield.Core.Payloads;
 using DevsDaddy.Shared.EventFramework;
 using UnityEngine;
@@ -12,6 +14,20 @@ namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
         private Options _currentOptions;
         private bool _initialized = false;
         private bool _isPaused = false;
+        
+        private const long TICKS_PER_SECOND = TimeSpan.TicksPerMillisecond * 1000;
+        private const int THRESHOLD = 5000000;
+        
+        private float m_Interval = 1f;
+        private byte m_MaxFalsePositives = 3;
+        private int m_CoolDown = 30;
+        
+        private byte m_CurrentFalsePositives;
+        private int m_CurrentCooldownShots;
+        private long m_TicksOnStart;
+        private long m_VulnerableTicksOnStart;
+        private long m_PrevTicks;
+        private long m_PrevIntervalTicks;
 
         /// <summary>
         /// Setup Module
@@ -32,13 +48,45 @@ namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
             if (!_initialized && !reinitialize)
                 Initialize();
         }
+        
+        /// <summary>
+        /// Initialize Module
+        /// </summary>
+        private void Initialize() {
+            // Setup from Config
+            m_Interval = _currentOptions.Interval;
+            m_MaxFalsePositives = _currentOptions.MaxFalsePositives;
+            m_CoolDown = _currentOptions.CoolDown;
+            
+            ResetStartTicks();
+            m_CurrentFalsePositives = 0;
+            m_CurrentCooldownShots = 0;
+            _isPaused = false;
+
+            // Fire Initialization Complete
+            EventMessenger.Main.Subscribe<ApplicationLoopUpdated>(OnGameLoopUpdate);
+            EventMessenger.Main.Publish(new SecurityModuleInitialized {
+                Module = this
+            });
+        }
+        
+        /// <summary>
+        /// Reset Startup Ticks
+        /// </summary>
+        private void ResetStartTicks()
+        {
+            m_TicksOnStart = DateTime.UtcNow.Ticks;
+            m_VulnerableTicksOnStart = System.Environment.TickCount * TimeSpan.TicksPerMillisecond;
+            m_PrevTicks = m_TicksOnStart;
+            m_PrevIntervalTicks = m_TicksOnStart;
+        }
 
         /// <summary>
         /// Disconnect Module
         /// </summary>
         public void Disconnect() {
-            
             // Fire Disconnected Complete
+            EventMessenger.Main.Unsubscribe<ApplicationLoopUpdated>(OnGameLoopUpdate);
             EventMessenger.Main.Publish(new SecurityModuleDisconnected {
                 Module = this
             });
@@ -51,10 +99,69 @@ namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
         public void PauseDetector(bool isPaused) {
             if(isPaused == _isPaused) return;
             _isPaused = isPaused;
+            if(!_isPaused) ResetStartTicks();
             EventMessenger.Main.Publish(new SecurityModulePause {
                 Module = this,
                 IsPaused = _isPaused
             });
+        }
+
+        /// <summary>
+        /// On Game Loop Updated
+        /// </summary>
+        /// <param name="payload"></param>
+        private void OnGameLoopUpdate(ApplicationLoopUpdated payload) {
+            if(_isPaused) return;
+            
+            long ticks = DateTime.UtcNow.Ticks;
+            long ticksSpentSinceLastUpdate = ticks - m_PrevTicks;
+            
+            if (ticksSpentSinceLastUpdate < 0 || ticksSpentSinceLastUpdate > TICKS_PER_SECOND)
+            {
+                if (Debug.isDebugBuild) Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} SpeedHack Detector: System DateTime change or > 1 second game freeze detected!");
+                ResetStartTicks();
+                return;
+            }
+            m_PrevTicks = ticks;
+            
+            long intervalTicks = (long)(m_Interval * TICKS_PER_SECOND);
+            if (ticks - m_PrevIntervalTicks >= intervalTicks)
+            {
+                long vulnerableTicks = System.Environment.TickCount * TimeSpan.TicksPerMillisecond;
+                if (Mathf.Abs((vulnerableTicks - m_VulnerableTicksOnStart) - (ticks - m_TicksOnStart)) > THRESHOLD)
+                {
+                    m_CurrentFalsePositives++;
+                    if (m_CurrentFalsePositives > m_MaxFalsePositives)
+                    {
+                        if (Debug.isDebugBuild) Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} SpeedHackDetector: final detection!");
+                        EventMessenger.Main.Publish(new SecurityWarningPayload {
+                            Code = 321,
+                            Message = SpeedHackWarnings.SpeedHackDetected,
+                            IsCritical = true,
+                            Module = this
+                        });
+                        PauseDetector(true);
+                    }
+                    else
+                    {
+                        if (Debug.isDebugBuild) Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} SpeedHackDetector: detection! Allowed false positives left: " + (m_MaxFalsePositives - m_CurrentFalsePositives));
+                        m_CurrentCooldownShots = 0;
+                        ResetStartTicks();
+                    }
+                }
+                else if (m_CurrentFalsePositives > 0 && m_CoolDown > 0)
+                {
+                    if (Debug.isDebugBuild) Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} SpeedHackDetector: success shot! Shots till Cooldown: " + (m_CoolDown - m_CurrentCooldownShots));
+                    m_CurrentCooldownShots++;
+                    if (m_CurrentCooldownShots >= m_CoolDown)
+                    {
+                        if (Debug.isDebugBuild) Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} SpeedHackDetector: Cooldown!");
+                        m_CurrentFalsePositives = 0;
+                    }
+                }
+
+                m_PrevIntervalTicks = ticks;
+            }
         }
 
         /// <summary>
@@ -63,17 +170,6 @@ namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
         /// <returns></returns>
         public bool IsPaused() {
             return _isPaused;
-        }
-
-        /// <summary>
-        /// Initialize Module
-        /// </summary>
-        private void Initialize() {
-
-            // Fire Initialization Complete
-            EventMessenger.Main.Publish(new SecurityModuleInitialized {
-                Module = this
-            });
         }
 
         /// <summary>
@@ -92,7 +188,9 @@ namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
         [System.Serializable]
         public class Options : IShieldModuleConfig
         {
-            
+            public float Interval = 1f;
+            public byte MaxFalsePositives = 3;
+            public int CoolDown = 30;
         }
     }
 }
---
+++ Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Modules/SpeedHack/SpeedHackWarnings.cs
@@ -0,0 +1,7 @@
+namespace DevsDaddy.GameShield.Core.Modules.SpeedHack
+{
+    public static class SpeedHackWarnings
+    {
+        public const string SpeedHackDetected = "The system detected a mismatch in the time intervals between the tacts of the game cycle. You may be using a game speed cheat.";
+    }
+}
