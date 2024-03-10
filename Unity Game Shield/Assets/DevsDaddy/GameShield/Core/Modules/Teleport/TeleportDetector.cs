0d3b689b29de888946bd28bb88403ec7b39f5948
Ilya Rastrorguev <i.rastorguev@outlook.com>
2024-03-10 21:04:10 +0500

    Add Teleport Detector Module

--- Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Modules/Teleport/TeleportDetector.cs
+++ Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Modules/Teleport/TeleportDetector.cs
@@ -1,3 +1,4 @@
+using DevsDaddy.GameShield.Core.Constants;
 using DevsDaddy.GameShield.Core.Payloads;
 using DevsDaddy.Shared.EventFramework;
 using UnityEngine;
@@ -12,6 +13,13 @@ namespace DevsDaddy.GameShield.Core.Modules.Teleport
         private Options _currentOptions;
         private bool _initialized = false;
         private bool _isPaused = false;
+        
+        private Transform _target;
+        private float availableSpeed = 3f;
+        private float timeToCheck = 1f;
+        private Vector3 lastPosition;
+
+        private float interval = 1f;
 
         /// <summary>
         /// Setup Module
@@ -32,16 +40,84 @@ namespace DevsDaddy.GameShield.Core.Modules.Teleport
             if (!_initialized && !reinitialize)
                 Initialize();
         }
+        
+        /// <summary>
+        /// Initialize Module
+        /// </summary>
+        private void Initialize() {
+            _target = _currentOptions.Target;
+            availableSpeed = _currentOptions.AvailableSpeed;
+            interval = _currentOptions.Interval;
+            timeToCheck = interval;
+
+            if (_target != null)
+                lastPosition = _target.transform.position;
+            
+            if(_target == null)
+                Debug.LogWarning($"{GeneralStrings.LOG_PREFIX} No Target setup to Teleport Detector. Please, use SetupTarget() to initialize detection.");
+
+            _isPaused = false;
+
+            // Fire Initialization Complete
+            EventMessenger.Main.Subscribe<ApplicationLoopUpdated>(OnGameLoopUpdate);
+            EventMessenger.Main.Publish(new SecurityModuleInitialized {
+                Module = this
+            });
+        }
+
+        /// <summary>
+        /// Setup Target
+        /// </summary>
+        /// <param name="target"></param>
+        /// <param name="maxSpeed"></param>
+        /// <param name="maxInterval"></param>
+        public void SetupTarget(Transform target, float maxSpeed = 30f, float maxInterval = 1f) {
+            _target = target;
+            availableSpeed = maxSpeed;
+            interval = maxInterval;
+            timeToCheck = maxInterval;
+        }
 
         /// <summary>
         /// Disconnect Module
         /// </summary>
         public void Disconnect() {
-            
-            // Fire Disconnected Complete
+            EventMessenger.Main.Unsubscribe<ApplicationLoopUpdated>(OnGameLoopUpdate);
             EventMessenger.Main.Publish(new SecurityModuleDisconnected {
                 Module = this
             });
+        }
+
+        /// <summary>
+        /// On Game Loop Updated
+        /// </summary>
+        /// <param name="payload"></param>
+        private void OnGameLoopUpdate(ApplicationLoopUpdated payload) {
+            if(_isPaused || _target == null) return;
+            
+            if (timeToCheck <= 0f)
+            {
+                if (lastPosition != Vector3.zero) {
+                    float distance = Vector3.Distance(lastPosition, _target.position);
+                    if (distance > availableSpeed)
+                    {
+                        EventMessenger.Main.Publish(new SecurityWarningPayload {
+                            Code = 422,
+                            Message = TeleportWarnings.TeleportCheating,
+                            IsCritical = true,
+                            Module = this
+                        });
+                        PauseDetector(true);
+                    }
+                }
+
+                lastPosition = _target.position;
+                timeToCheck = interval;
+            }
+            else
+            {
+                timeToCheck -= payload.DeltaTime;
+            }
         }
         
         /// <summary>
@@ -66,17 +142,6 @@ namespace DevsDaddy.GameShield.Core.Modules.Teleport
         }
 
         /// <summary>
-        /// Initialize Module
-        /// </summary>
-        private void Initialize() {
-
-            // Fire Initialization Complete
-            EventMessenger.Main.Publish(new SecurityModuleInitialized {
-                Module = this
-            });
-        }
-
-        /// <summary>
         /// Get Module Information
         /// </summary>
         /// <returns></returns>
@@ -92,7 +157,9 @@ namespace DevsDaddy.GameShield.Core.Modules.Teleport
         [System.Serializable]
         public class Options : IShieldModuleConfig
         {
-            
+            public Transform Target;
+            public float AvailableSpeed = 30f;
+            public float Interval = 1f;
         }
     }
 }
