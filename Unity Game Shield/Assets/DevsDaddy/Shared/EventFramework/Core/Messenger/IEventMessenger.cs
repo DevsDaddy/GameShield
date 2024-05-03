53dc90acbd50f4a46e93dd07096bca428099db2e
Ilya Rastrorguev <i.rastorguev@outlook.com>
2024-05-03 12:40:21 +0500

    Change Event Framework to Git Depedency

--- Unity Game Shield/Assets/DevsDaddy/Shared/EventFramework/Core/Messenger/IEventMessenger.cs
+++
@@ -1,7 +0,0 @@
-namespace DevsDaddy.Shared.EventFramework.Core.Messenger
-{
-    /// <summary>
-    /// Event Messenger Interface
-    /// </summary>
-    public interface IEventMessenger : IEventMessengerPublish, IEventMessengerSubscribe, IEventMessengerUnsubscribe { }
-}
