85960f9ed9905c52196ae4acb0e0bb7a7b01a9b8
PhantomOSTeam <65129208+PhantomOSTeam@users.noreply.github.com>
2025-01-14 16:03:27 +0100

    Removed unnecessary script

--- Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Editor/PropertyDrawers/SecuredPropertyDrawer.cs
+++ Unity Game Shield/Assets/DevsDaddy/GameShield/Core/Editor/PropertyDrawers/SecuredPropertyDrawer.cs
@@ -1,20 +1,4 @@
-﻿/*
- * Pixel Anti Cheat
- * ======================================================
- * This library allows you to organize a simple anti-cheat
- * for your game and take care of data security. You can
- * use it in your projects for free.
- *
- * Note that it does not guarantee 100% protection for
- * your game. If you are developing a multiplayer game -
- * never trust the client and check everything on
- * the server.
- * ======================================================
- * @developer       TinyPlay
- * @author          Ilya Rastorguev
- * @url             https://github.com/TinyPlay/Pixel-Anticheat
- */
-namespace PixelSecurity.Editor.PropertyDrawers
+﻿namespace PixelSecurity.Editor.PropertyDrawers
 {
     using System.Reflection;
     using UnityEditor;
